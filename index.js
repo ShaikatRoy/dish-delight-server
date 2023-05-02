7b472477d05963d22e6f66305a6505ad0d12c954
Shaikat roy <shaikatroy258@gmail.com>
2023-05-03 03:18:33 +0600

    data ready to used in client side

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+const chefs = require('./Data/chefs.json');
+const recipes = require('./Data/recipes.json');
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Server is running')
+});
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
+});
+
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChefs = chefs.find(n=> n.id == id);
+    res.send(selectedChefs);
+});
+
+app.get('/recipes/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedRecipes = recipes.filter(n => n.chef_id == id);
+    res.send(selectedRecipes);
+})
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes);
 });
 
 app.listen(port, () => {
